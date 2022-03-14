17201fab54b0fffba04f5e4801d52863388aa188
Vladislav Likhachev <mymindissharp@gmail.com>
2022-03-14 21:57:01 +0300

    added solution

--- src/main/java/com/example/rest/configuration/AuthorityConfiguration.java
+++ src/main/java/com/example/rest/configuration/AuthorityConfiguration.java
@@ -1,22 +1,39 @@
 package com.example.rest.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class AuthorityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder().encode("admin"))
+                .roles("ADMIN")
                 .and()
-                .authorizeRequests()
-                .antMatchers("/persons/by-city")
-                .permitAll()
+                .withUser("man")
+                .password(encoder().encode("man"))
+                .roles("READ")
                 .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated();
+                .withUser("women")
+                .password(encoder().encode("women"))
+                .roles("WRITE")
+                .and()
+                .withUser("child")
+                .password(encoder().encode("child"))
+                .roles("DELETE");
     }
 }
--- src/main/java/com/example/rest/controller/PersonsController.java
+++ src/main/java/com/example/rest/controller/PersonsController.java
@@ -4,8 +4,11 @@ import com.example.rest.model.Persons;
 import com.example.rest.service.PersonsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.io.InvalidObjectException;
 import java.util.List;
 
@@ -15,19 +18,28 @@ import java.util.List;
 public class PersonsController {
     PersonsService service;
 
+    @PreAuthorize("hasAnyRole('DELETE')")
     @GetMapping("/persons/by-city")
     public List<Persons> getPersonsByCity(@RequestParam("city") String city) throws Exception {
         return service.getPersonsByCity(city);
     }
 
+    @RolesAllowed("ROLE_WRITE")
     @GetMapping("/persons/by-age")
     public List<Persons> getPersonsByCity(@RequestParam("age") int age) throws Exception {
         return service.getPersonsByAge(age);
     }
 
+    @Secured("ROLE_READ")
     @GetMapping("/persons/by-name-surname")
     public Persons getPersonsByCity(@RequestParam("name") String name, @RequestParam("surname") String surname) throws Exception {
         return service.getPersonsByNameAndSurname(name, surname);
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/admin")
+    public String admin(String username) throws Exception {
+        return "YOU ARE ADMIN!";
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
